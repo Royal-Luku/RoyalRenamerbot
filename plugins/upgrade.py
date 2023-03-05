73cefab0671cae5655ec5cc9025eacea7790582e
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-03-05 06:37:55 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -24,10 +24,9 @@ async def upgrade(bot,update):
 	
 	After Payment Done Send Screenshots To Admin!"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("Admin",url = "https://t.me/royaldwip")], 
-        			[InlineKeyboardButton("Chek Plan",callback_data = "plan")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
-	await update.message.edit(text = text,reply_markup = keybord)
-	
+        			InlineKeyboardButton("Send Screenshot",url = "https://t.me/Royaldwip")], 
+        			[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await message.reply_text(text = text,reply_markup = keybord)
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
@@ -51,6 +50,6 @@ async def upgradecm(bot,message):
 	
 	After Payment Done Send Screenshots To Admin!"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN",url = "https://t.me/OTT_Zone_Admin")],
-        			[InlineKeyboardButton("chek Plan",callback_data = "plan")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("Send Screenshot",url = "https://t.me/Royaldwip")], 
+        			[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
